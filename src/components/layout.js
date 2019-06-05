561ec26557df0d5602a64bbcd8ad814fdb230c64
David Öhlin <hello@davidohl.in>
2019-06-05 09:53:39 +0200

    reformat with prettier :pig:

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,51 +5,51 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
 import '../css/index.css'
 
-import Header from "./header"
+import Header from './header'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          className="bg-blue"
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+	<StaticQuery
+		query={graphql`
+			query SiteTitleQuery {
+				site {
+					siteMetadata {
+						title
+					}
+				}
+			}
+		`}
+		render={data => (
+			<>
+				<Header siteTitle={data.site.siteMetadata.title} />
+				<div
+					className="bg-blue"
+					style={{
+						margin: `0 auto`,
+						maxWidth: 960,
+						padding: `0px 1.0875rem 1.45rem`,
+						paddingTop: 0,
+					}}
+				>
+					<main>{children}</main>
+					<footer>
+						© {new Date().getFullYear()}, Built with
+						{` `}
+						<a href="https://www.gatsbyjs.org">Gatsby</a>
+					</footer>
+				</div>
+			</>
+		)}
+	/>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
