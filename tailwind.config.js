35d193e250ebae1d70d2828f4e24ae72e25c0bc6
David Öhlin <hello@davidohl.in>
2019-06-10 17:01:44 +0200

    add proxima nova from typekit :tulip:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,23 @@
 // https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 
 module.exports = {
-  
+	theme: {
+		fontFamily: {
+			sans: [
+				'proxima-nova',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'"Segoe UI"',
+				'Roboto',
+				'"Helvetica Neue"',
+				'Arial',
+				'"Noto Sans"',
+				'sans-serif',
+				'"Apple Color Emoji"',
+				'"Segoe UI Emoji"',
+				'"Segoe UI Symbol"',
+				'"Noto Color Emoji"',
+			],
+		},
+	},
 }
