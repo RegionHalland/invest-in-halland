5715ff6c3d4f61c7bdd9ef5c5584f371106a5a32
David Öhlin <hello@davidohl.in>
2019-06-17 10:47:32 +0200

    add tailwind to storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = ({ config, mode }) => {
+	config.module.rules.push({
+		test: /\.css$/,
+		loaders: [
+			{
+				loader: 'postcss-loader',
+				options: {
+					sourceMap: true,
+					config: { path: './.storybook/' },
+				},
+			},
+		],
+		include: path.resolve(__dirname, '../'),
+	})
+
+	return config
+}
