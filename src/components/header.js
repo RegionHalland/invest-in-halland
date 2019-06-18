ffeaa7542b9d0032d7c255cf9bbd9c8748e96ce0
David Öhlin <hello@davidohl.in>
2019-06-18 10:47:28 +0200

    add contact to header

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,21 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { useNavigationItems } from '../hooks/useNavigationItems'
 
-const Header = ({ siteTitle }) => {
+export default ({ siteTitle }) => {
 	const { items: navigation } = useNavigationItems()
 	return (
-		<header>
+		<header class="mb-6 p-3 border-b flex justify-between">
 			<h1>Invest in Halland</h1>
 			<nav>
 				{navigation.map(item => (
-					<Link key={item.wordpress_id} to="/">
+					<Link key={item.wordpress_id} to="/" className="ml-3">
 						{item.title}
 					</Link>
 				))}
+				<Link to="/contact" className="ml-3">
+					Kontakta oss
+				</Link>
 			</nav>
 		</header>
 	)
 }
-
-export default Header
