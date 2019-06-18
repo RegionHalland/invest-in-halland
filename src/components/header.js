7378bed59f4264cf3191906edf2337fbe23a7b4a
David Öhlin <hello@davidohl.in>
2019-06-18 10:43:25 +0200

    add navigation hook :hocho:

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import { useNavigationItems } from '../hooks/useNavigationItems'
 
-const Header = () => (
-	<header>
-		<div>
-			<span>Invest in Halland</span>
-		</div>
-	</header>
-)
+const Header = ({ siteTitle }) => {
+	const { items: navigation } = useNavigationItems()
+	return (
+		<header>
+			<h1>Invest in Halland</h1>
+			<nav>
+				{navigation.map(item => (
+					<Link key={item.wordpress_id} to="/">
+						{item.title}
+					</Link>
+				))}
+			</nav>
+		</header>
+	)
+}
 
 export default Header
---
+++ src/hooks/useNavigationItems.js
@@ -0,0 +1,17 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+export const useNavigationItems = () => {
+	const { wordpressWpApiMenusMenusItems } = useStaticQuery(graphql`
+		{
+			wordpressWpApiMenusMenusItems(name: { eq: "Huvudmeny" }) {
+				items {
+					wordpress_id
+					order
+					title
+				}
+			}
+		}
+	`)
+
+	return wordpressWpApiMenusMenusItems
+}
