c8cf8429054d04bb8353a557cd0c475d0e2a4082
David Öhlin <hello@davidohl.in>
2019-06-19 14:33:54 +0200

    create pages for navigation items

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,28 @@
 const path = require('path')
 const slash = require('slash')
+const slugify = require('slugify')
 
 exports.createPages = async ({ graphql, actions }) => {
 	const { createPage } = actions
 
-	const statisticSingle = path.resolve('./src/templates/statistic-single.js')
+	const statistics = path.resolve('./src/templates/statistics.js')
+	const statisticsSingle = path.resolve(
+		'./src/templates/statistics-single.js'
+	)
+	const articles = path.resolve('./src/templates/articles.js')
+	const articlesSingle = path.resolve('./src/templates/articles-single.js')
 
-	// Create Single Statistic Pages
-	const statisticSingleResult = await graphql(`
+	const templates = {
+		statistic: statistics,
+		statisticsSingle: statisticsSingle,
+		article: articles,
+		articlesSingle: articlesSingle,
+	}
+
+	/**
+	 * Create Single Statistic Pages
+	 */
+	const statisticsSingleResult = await graphql(`
 		{
 			allWordpressWpStatistic {
 				edges {
@@ -20,14 +35,49 @@ exports.createPages = async ({ graphql, actions }) => {
 		}
 	`)
 
-	statisticSingleResult.data.allWordpressWpStatistic.edges.forEach(edge => {
+	statisticsSingleResult.data.allWordpressWpStatistic.edges.forEach(edge => {
 		createPage({
 			path: `statistik/${edge.node.slug}`,
-			component: slash(statisticSingle),
+			component: slash(statisticsSingle),
 			context: {
 				id: edge.node.id,
 				slug: edge.node.slug,
 			},
 		})
 	})
+
+	/**
+	 * Create pages for all navigation items
+	 */
+	const navigationItemsResult = await graphql(`
+		{
+			allWordpressWpApiMenusMenusItems {
+				edges {
+					node {
+						items {
+							wordpress_id
+							title
+							description
+							object
+						}
+					}
+				}
+			}
+		}
+	`)
+
+	navigationItemsResult.data.allWordpressWpApiMenusMenusItems.edges
+		.map(edge => edge.node.items)
+		.reduce((pre, current) => pre.concat(current))
+		.filter(item => item.object !== 'custom')
+		.forEach(item => {
+			createPage({
+				path: slugify(item.title, { lower: true }),
+				component: slash(templates[item.object]),
+				context: {
+					wordpress_id: item.object_id,
+					slug: item.object,
+				},
+			})
+		})
 }
---
+++ src/templates/articles-single.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../layouts/Default'
+import SEO from '../components/Seo'
+
+export default () => (
+	<Layout>
+		<SEO title="hi" />
+		<div>hi</div>
+	</Layout>
+)
