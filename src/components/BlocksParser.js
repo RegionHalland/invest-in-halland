32d5343b824fda63091d2bf3e541cabee9ea6d62
sebastiansson <sebastian.marcusson@gmail.com>
2019-08-13 09:02:35 +0200

    Remove reference to TwoColumn

--- src/components/BlocksParser.js
+++ src/components/BlocksParser.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactHtmlParser from 'react-html-parser'
 import Fact from './blocks/Fact'
-import TwoColumn from './blocks/TwoColumn'
 
 const Components = {
 	'acf/fact': Fact,
-	'acf/two-column': TwoColumn,
 }
 
 const BlocksParser = ({ blocks }) => {
 	return blocks
 		.filter(block => block.blockName !== null)
 		.map(block => {
-			console.log('asda', block)
+			console.log('block', block)
 			const Component = Components[block.blockName]
 			return block.blockName.includes('core/') ? (
 				ReactHtmlParser(block.innerHTML)
