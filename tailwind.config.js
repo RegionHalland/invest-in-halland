05c944bf19a33ecad865c81dc2c1b4f06223c123
David Öhlin <hello@davidohl.in>
2019-08-20 08:52:22 +0200

    update tailwind colors from figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,33 @@ module.exports = {
 				'"Noto Color Emoji"',
 			],
 		},
+		extend: {
+			colors: {
+				black: '#1B1B1B',
+				gray: {
+					200: '#F4F5F9',
+					300: '#E8EBF4',
+					400: '#CACDD7',
+					500: '#898D98',
+					600: '#5C666D',
+					700: '#33353C',
+				},
+				red: {
+					500: '#E1807A',
+				},
+				green: {
+					500: '#5CB78A',
+					400: '#82C3A1',
+				},
+				blue: {
+					600: '#094061',
+					500: '#1D506F',
+					400: '#3F6F8C',
+					300: '#6F95AD',
+					200: '#D1E4EF',
+				},
+			},
+		},
 	},
 	plugins: [require('@digitaliseringsbyran/tailwindcss-screens-in-dom')()],
 }
