e5bd936ec937a235c16039bb38e0ec64fb927ba7
sebastiansson <sebastian.marcusson@gmail.com>
2019-08-22 14:16:05 +0200

    Add new conditionals for component rendering

--- src/components/RelatedArticles.js
+++ src/components/RelatedArticles.js
@@ -1,12 +1,7 @@
 import React from 'react'
 
 import ArticleCard from './ArticleCard'
-
-const Components = {
-	company_story: ArticleCard,
-	fact: ArticleCard,
-	opportunity: ArticleCard,
-}
+import FactCard from './FactCard'
 
 const RelatedArticles = ({ articles }) => (
 	<React.Fragment>
@@ -14,28 +9,57 @@ const RelatedArticles = ({ articles }) => (
 			<h3 className="font-semibold text-2xl">Relaterat innehåll</h3>
 		</div>
 		<div className="flex flex-wrap w-full">
-			{articles.map(related_article => {
+			{articles.map((related_article, key) => {
 				let article = related_article.related_article
-				let Component = Components[article.post_type]
-				return (
-					<div
-						className="w-full md:w-6/12 px-3"
-						key={article.wordpress_id}
-					>
-						<Component
-							ratio="1:1"
-							title={article.post_title}
-							img={
-								article.featured_media
-									? article.featured_media.localFile
-											.childImageSharp.fluid
-									: null
-							}
-							category={article.area_name}
-							url={article.url}
-						/>
-					</div>
-				)
+				let post_type = article.post_type
+				if (
+					post_type === 'opportunity' ||
+					post_type === 'company_story'
+				) {
+					return (
+						<div
+							className="w-full md:w-6/12 px-3"
+							key={article.wordpress_id}
+						>
+							<ArticleCard
+								key={key}
+								ratio="1:1"
+								title={article.post_title}
+								url={article.url}
+								img={
+									article.featured_media
+										? article.featured_media.localFile
+												.childImageSharp.fluid
+										: null
+								}
+							/>
+						</div>
+					)
+				}
+				if (post_type === 'fact') {
+					return (
+						<div
+							className="w-full md:w-6/12 px-3"
+							key={article.wordpress_id}
+						>
+							<FactCard
+								key={key}
+								alignment="center"
+								title={article.post_title}
+								url={article.url}
+								contactName={article.contact.post_title}
+								contactCompany={article.contact.acf.company}
+								img={
+									article.contact.acf.featured_media
+										? article.contact.acf.featured_media
+												.localFile.childImageSharp.fixed
+										: null
+								}
+							/>
+						</div>
+					)
+				}
+				return null
 			})}
 		</div>
 	</React.Fragment>
