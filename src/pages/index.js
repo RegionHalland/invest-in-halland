d67afc56bb8541f756e47c1897b83c9d34453931
sebastiansson <sebastian.marcusson@gmail.com>
2019-08-26 16:51:11 +0200

    WIP add startpage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import ReactHtmlParser from 'react-html-parser'
 
 import Layout from '../layouts/Default'
 import SEO from '../components/Seo'
+import HeroWithPost from '../components/HeroWithPost'
+import ArticleGrid from '../components/ArticleGrid'
 
 const IndexPage = ({
 	data: {
@@ -20,13 +22,17 @@ const IndexPage = ({
 	},
 }) => (
 	<Layout>
+		<SEO title="Home" />
+		<HeroWithPost
+			title={title + '<br />' + words[0].word}
+			textAlign="center"
+		/>
 		<div className="container mx-auto z-10">
-			<SEO title="Home" />
-			<h1 className="text-5xl">
-				{title}
-				{words[0].word}
-			</h1>
-			<p>{ReactHtmlParser(introduction_text)}</p>
+			<div className="py-20 w-2/4 mx-auto text-center">
+				{ReactHtmlParser(introduction_text)}
+			</div>
+
+			<ArticleGrid articles={featured_articles} />
 		</div>
 	</Layout>
 )
@@ -42,7 +48,22 @@ export const query = graphql`
 						word
 					}
 					featured_articles {
-						post_title
+						title
+						area {
+							name
+						}
+						featured_media {
+							title
+							caption
+							alt_text
+							localFile {
+								childImageSharp {
+									fluid(maxWidth: 1920) {
+										...GatsbyImageSharpFluid_withWebp
+									}
+								}
+							}
+						}
 					}
 				}
 			}
