e27ea3af3fdf4d433d094a4b10bf9329f2ee2a7f
sebastiansson <sebastian.marcusson@gmail.com>
2019-08-26 15:11:15 +0200

    Add start page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import ReactHtmlParser from 'react-html-parser'
 
 import Layout from '../layouts/Default'
 import SEO from '../components/Seo'
 
-const IndexPage = ({ data }) => (
+const IndexPage = ({
+	data: {
+		wordpressAcfOptions: {
+			options: {
+				startpage: {
+					introduction_text,
+					title,
+					featured_articles,
+					words,
+				},
+			},
+		},
+	},
+}) => (
 	<Layout>
 		<div className="container mx-auto z-10">
 			<SEO title="Home" />
-			<h1 className="text-5xl">Invest in Halland</h1>
-			<p></p>
+			<h1 className="text-5xl">
+				{title}
+				{words[0].word}
+			</h1>
+			<p>{ReactHtmlParser(introduction_text)}</p>
 		</div>
 	</Layout>
 )
