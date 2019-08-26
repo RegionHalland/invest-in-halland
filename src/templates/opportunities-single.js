f18da0bdfe7dc1653df63bea119e3d4239bf9b29
David Öhlin <hello@davidohl.in>
2019-08-26 12:44:34 +0200

    parse content in opportunities-single

--- src/templates/opportunities-single.js
+++ src/templates/opportunities-single.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+
 import Layout from '../layouts/Default'
+import Article from '../components/Article'
+import HeroWithPost from '../components/HeroWithPost'
 import SEO from '../components/Seo'
-import BlockParser from '../components/BlocksParser'
 
 export default ({
 	data: {
-		wordpressWpOpportunity: { title, blocks },
+		wordpressWpOpportunity: { title, area, blocks, acf },
 	},
-}) => (
-	<Layout>
-		<div className="container mx-auto z-10">
+}) => {
+	const subtitle = area && area.length ? area[0].name : null
+
+	return (
+		<Layout>
 			<SEO title={title} />
-			<div>Opportunity {title}</div>
-			<BlockParser blocks={blocks} />
-		</div>
-	</Layout>
-)
+			<HeroWithPost title={title} subtitle={subtitle} />
+			<div className="mx-auto container py-12 md:py-16 lg:py-24">
+				<div className="mx-auto w-full md:w-10/12 lg:w-7/12 px-3">
+					<Article blocks={blocks} acf={acf} />
+				</div>
+			</div>
+		</Layout>
+	)
+}
 
 export const query = graphql`
 	query($slug: String!) {
