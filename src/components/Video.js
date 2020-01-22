72a6ff62ed0a13b9f84dd75d562d3582a5486540
Albin Martinsson <albin@digitaliseringsbyran.se>
2020-01-22 11:23:52 +0100

    Create Video.js

---
+++ src/components/Video.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
+
+const EmbeddedVideo = ({ videoSrcURL, videoTitle, ...props }) => (
+	<div className="">Tjenare</div>
+)
