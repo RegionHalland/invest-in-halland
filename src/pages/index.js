cdf1444f624c900b42cbe0707597a84585ec66cb
Albin Martinsson <albin@digitaliseringsbyran.se>
2020-01-22 11:21:33 +0100

    Add video

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import ArticleCard from '../components/ArticleCard'
 import FactCard from '../components/FactCard'
 import MapCard from '../components/MapCard'
 import Button from '../components/Button'
+import EmbeddedVideo from '../components/EmbeddedVideo'
 
 const IndexPage = () => {
 	const {
@@ -46,6 +47,10 @@ const IndexPage = () => {
 					<h2 className="text-2xl md:text-4xl font-bold mb-3">
 						{featured_articles_title}
 					</h2>
+					<EmbeddedVideo
+						videoSrcURL="https://www.youtube.com/embed/2sSGlE1jIAA"
+						title="Nothing but opportunities - video"
+					/>
 					<div>
 						{ReactHtmlParser(featured_articles_introduction_text)}
 					</div>
