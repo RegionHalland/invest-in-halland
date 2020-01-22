569c0a43dc4fa7cb88c199e00e5c3f79c98f161c
Albin Martinsson <albin@digitaliseringsbyran.se>
2020-01-22 11:24:15 +0100

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,5 +4,15 @@ import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
 const EmbeddedVideo = ({ videoSrcURL, videoTitle, ...props }) => (
-	<div className="">Tjenare</div>
+	<div className="w-full lg:w-10/12 mx-auto mb-5">
+		<StyledIframe
+			src={videoSrcURL}
+			title={videoTitle}
+			allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+			frameBorder="0"
+			webkitallowfullscreen="true"
+			mozallowfullscreen="true"
+			allowFullScreen
+		/>
+	</div>
 )
